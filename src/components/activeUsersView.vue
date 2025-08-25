3f2cf211aa4eda78cc8f34c21bd6bff405a4aa41
jorgeobnd <ryuuzaki.dev@gmail.com>
2025-08-25 12:02:00 -0600

    reade Users collection

---
+++ src/components/activeUsersView.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-page-sticky position="top" expand>
+    <q-tabs
+      v-model="userSelect"
+      inline-label
+      outside-arrows
+      mobile-arrows
+      class="bg-primary text-white shadow-2 full-width"
+    >
+      <q-tab
+        v-for="user in users"
+        :key="user.id"
+        :name="user.uid"
+        icon="account_circle"
+        :label="user.mail"
+      />
+    </q-tabs>
+  </q-page-sticky>
+</template>
+
+<script setup>
+import { onMounted, onUnmounted, ref } from 'vue'
+import { db } from 'boot/firebase.js'
+import { collection, onSnapshot } from 'firebase/firestore'
+const userSelect = ref('mails')
+const users = ref([])
+
+onMounted(() => {
+  // get reference to "users" collection
+  const usersCollection = collection(db, 'users')
+
+  // listen in real-time
+  const unsubscribe = onSnapshot(usersCollection, (snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      if (change.type === 'added') {
+        console.log('New user:', change.doc.data())
+        users.value = [...users.value, change.doc.data()]
+      }
+      if (change.type === 'modified') {
+        console.log('Modified user:', change.doc.data())
+        users.value = users.value.map((user) =>
+          user.uid === change.doc.data().uid ? { ...user, state: change.doc.data().state } : user,
+        )
+      }
+      if (change.type === 'removed') {
+        console.log('Removed user:', change.doc.data())
+      }
+    })
+  })
+
+  // clean up listener when component unmounts
+  onUnmounted(() => unsubscribe())
+})
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,9 @@
 <template>
   <q-page padding>
     <access-view v-if="!isAuthenticated" />
+    <div v-else>
+      <active-users-view/>
+    </div>
   </q-page>
 </template>
 
@@ -8,6 +11,7 @@
 import AccessView from 'components/accessView.vue'
 import { getAuth } from 'firebase/auth'
 import { useAuth } from '@vueuse/firebase'
+import ActiveUsersView from 'components/activeUsersView.vue'
 
 const auth = getAuth()
 const { isAuthenticated } = useAuth(auth)
